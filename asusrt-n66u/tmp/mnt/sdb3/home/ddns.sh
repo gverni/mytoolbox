5f0bf29c7dc7af13333d4a4681063031518d84ac
Giuseppe@Thinkpad <g.verni@gmail.com>
2016-02-15 22:46:07 +0000

    ddns.sh: moved pushbullet token to external file

--- asusrt-n66u/tmp/mnt/sdb3/home/ddns.sh
+++ asusrt-n66u/tmp/mnt/sdb3/home/ddns.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 OLDIP=$(cat /tmp/mnt/sdb3/home/ip.txt) 
 IP=$(curl -s icanhazip.com)
+PBTOKEN=$(cat pbtoken.txt)
 
 if [ "$IP" != "" ]
 then
 echo "$IP" > /tmp/mnt/sdb3/home/ip.txt
 if [ "$OLDIP" != "$IP" ] 
 then
-  curl --header 'Authorization: Bearer v1b98AJISxZq5xzSHDs9qXh5qjPiQYv7pYujAAuwIbrsi' -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "New IP", "body": "'$IP'"}'
+curl --header "Authorization: Bearer $PBTOKEN" -X POST https://api.pushbullet.com/v2/pushes --header 'Content-Type: application/json' --data-binary '{"type": "note", "title": "New IP", "body": "'$IP'"}'
 fi
 fi 
